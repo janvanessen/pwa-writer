73b1e10b667cb7af61a7106d81dd5615d08babf5
janvanessen <jan.vanessen@babelo.de>
2020-06-17 17:22:46 +0200

    init

---
+++ build/precache-manifest.dfdc4cd7622e791d1055ab0785822ec7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab98028521b80f0df441c6120b28075a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88d79ce6dceff2f17c40",
+    "url": "/static/css/main.30c6f206.chunk.css"
+  },
+  {
+    "revision": "6211dfaff0b05b0a6793",
+    "url": "/static/js/2.b04f4cc5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04f4cc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88d79ce6dceff2f17c40",
+    "url": "/static/js/main.0fc76d78.chunk.js"
+  },
+  {
+    "revision": "10dce9aac6f17f09f775",
+    "url": "/static/js/runtime-main.f35dcbd9.js"
+  }
+]);
---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="editor">
+          <textarea placeholder="Start typing...">
+
+          </textarea>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
